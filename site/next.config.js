81b7a63968fea6b436f1184fd0a511a3e853e5db
MrGasha <62189571+MrGasha@users.noreply.github.com>
2024-10-02 19:52:59 -0500

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -3,14 +3,14 @@
 const nextConfig = {
   env: {
     name: 'MrGasha Registry',
-    description: 'My custom workspaces',
+    description: 'My favorites custom workspaces',
     icon: '/img/logo.svg',
     listUrl: 'https://mrgasha.github.io/mrgasha-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/mrgasha-registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
