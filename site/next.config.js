565ecdd9f7a9077062410b22bbe4b64565e890c3
MrGasha <62189571+MrGasha@users.noreply.github.com>
2025-10-25 22:14:40 -0500

    Update next.config.js
    some testing

--- site/next.config.js
+++ site/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: 'export',
+  distDir: '../public',
   env: {
     name: 'MrGasha Registry',
     description: 'My favorites custom workspaces',
